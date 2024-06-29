7a8500dac9160559eaac3f6d82478960563de9d2
erendoru <erendoru@hotmail.com>
2024-06-29 22:56:36 +0300

    projects section added

--- app/page.tsx
+++ app/page.tsx
@@ -2,26 +2,23 @@ import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/floating-navbar";
 import { FaHome } from "react-icons/fa";
-import { AnimatedPinDemo } from "@/components/Pin";
 import { LinkPreviewDemo } from "@/components/Preview";
+import { HeroParallax } from "@/components/ui/hero-parallax";
+import { projects } from "@/data";
+import { navItems } from "@/data";
 
 export default function Home() {
+  const products = [];
+
   return (
     <main className="relative  bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full ">
-        <FloatingNav
-          navItems={[
-            {
-              name: "Home",
-              link: "/",
-              icon: <FaHome />,
-            },
-          ]}
-        />
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <LinkPreviewDemo />
-        <AnimatedPinDemo />
+        <HeroParallax products={projects} />
+        <h1>sa</h1>
       </div>
     </main>
   );
