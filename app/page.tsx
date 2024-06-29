0d9937c09325d29d12113947e22835123af6cca1
erendoru <erendoru@hotmail.com>
2024-06-29 13:43:15 +0300

    fixed

---
+++ app/page.tsx
@@ -0,0 +1,28 @@
+import Grid from "@/components/Grid";
+import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/floating-navbar";
+import { FaHome } from "react-icons/fa";
+import { AnimatedPinDemo } from "@/components/Pin";
+import { LinkPreviewDemo } from "@/components/Preview";
+
+export default function Home() {
+  return (
+    <main className="relative  bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full ">
+        <FloatingNav
+          navItems={[
+            {
+              name: "Home",
+              link: "/",
+              icon: <FaHome />,
+            },
+          ]}
+        />
+        <Hero />
+        <Grid />
+        <LinkPreviewDemo />
+        <AnimatedPinDemo />
+      </div>
+    </main>
+  );
+}
