160b5a2d1bbc907edbddc1c39a19165f6eb563fd
erendoru <erendoru@hotmail.com>
2024-06-30 03:50:11 +0300

    social media added

---
+++ components/SocialMedia.tsx
@@ -0,0 +1,24 @@
+import { socialMedia } from "../data";
+import Link from "next/link";
+import React from "react";
+
+const SocialMedia = () => {
+  return (
+    <div>
+      <div className="flex items-center justify-end md:gap-3 gap-6 my-5 ">
+        {socialMedia.map((info) => (
+          <div
+            key={info.id}
+            className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+          >
+            <Link rel="stylesheet" href={info.link}>
+              <img src={info.img} alt="icons" width={20} height={20} />
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SocialMedia;
