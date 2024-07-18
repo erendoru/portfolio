c084bc22503173dd7cda423b022ad0fb93a00059
erendoru <erendoru@hotmail.com>
2024-07-18 04:19:28 +0300

    blog page added

---
+++ app/blog/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function blogpage() {
+  return (
+    <div className="bg-white-200 flex justify-center align-middle ">
+      <h1>Blog Page</h1>
+    </div>
+  );
+}
+
+export default blogpage;
